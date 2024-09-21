71cf43323c221d1cc92f0718351ee1285c1f768a
AhmadMudasirStanikzay <ahmad.mudasir.stanikzay@iths.se>
2024-09-21 15:13:38 +0200

    Finished Laboration1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
 
+    private static String[] args;
+
     public static void main(String[] args) {
-        System.out.println("Hello There!");
-    }
+        App.args = args;
+        System.out.println("Hello There"); }
 }
+
